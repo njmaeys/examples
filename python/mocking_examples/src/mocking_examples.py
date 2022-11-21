e57a6815d94d7dcc358a57ce4d5fbc1ec8182641
njmaeys <nmaeys@hotmail.com>
2022-11-21 12:40:30 -0600

    remove prints

--- python/mocking_examples/src/mocking_examples.py
+++ python/mocking_examples/src/mocking_examples.py
@@ -1,9 +1,7 @@
 def older_times_two(age: int) -> int:
-    print('HERE: person older times two')
     new_age = age * 2
     return new_age
 
 def person_older(name: str, age: int) -> str:
-    print('HERE: person older')
     age_times_two = older_times_two(age)
     return f"{name} is {age_times_two}"
