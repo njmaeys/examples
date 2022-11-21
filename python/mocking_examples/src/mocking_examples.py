4e0c73eb5a1948f941a57cdd11ea4d996b8d3e30
njmaeys <nmaeys@hotmail.com>
2022-11-21 09:51:02 -0600

    adding the assertion of calling a function

--- python/mocking_examples/src/mocking_examples.py
+++ python/mocking_examples/src/mocking_examples.py
@@ -1,7 +1,9 @@
 def older_times_two(age: int) -> int:
+    print('HERE: person older times two')
     new_age = age * 2
     return new_age
 
 def person_older(name: str, age: int) -> str:
+    print('HERE: person older')
     age_times_two = older_times_two(age)
     return f"{name} is {age_times_two}"
--- python/mocking_examples/tests/mocking_test.py
+++ python/mocking_examples/tests/mocking_test.py
@@ -11,10 +11,16 @@ def test_older_times_two():
 def test_with_a_mock_person_older(
     mock_older_times_two
 ):
-    # This patch value here is on the return of the expected function
+    # this patch value here is on the return of the expected function
     # which shows that 99999 overwrites the supplied 31 * 2 = 62 to
     # older_times_two when the function runs
     mock_older_times_two.return_value = 99999
 
+    # actually call the logic you want
     result = mocking_examples.person_older("Nate", 31)
+
+    # assert function(s) were called even when patched
+    # this must be called after you run the function(s)
+    mock_older_times_two.assert_called_once_with(31)
+
     assert result == "Nate is 99999"
